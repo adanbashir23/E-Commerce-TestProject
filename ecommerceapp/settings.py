6a0fe0f2e571ef40a69322faf97dceb366ac1e85
Adan Bashir <adan.bashir@devsinc.com>
2022-11-25 11:42:03 +0500

    updated

--- ecommerceapp/settings.py
+++ ecommerceapp/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["e-commerce-testproject.herokuapp.com"]
 
 AUTH_USER_MODEL = "userprofile.UserProfile"
 
