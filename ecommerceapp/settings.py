448cc6947c72850222fc8068557ba95825d4bec8
Adan Bashir <adan.bashir@devsinc.com>
2022-11-25 13:00:17 +0500

    updated

--- ecommerceapp/settings.py
+++ ecommerceapp/settings.py
@@ -169,7 +169,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
-STATIC_ROOT = "staticfiles"
+STATIC_ROOT = "/ecommerceapp/static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
