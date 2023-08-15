14faf81d7c8190fe7391e1d5cd909b801270a448
Wintogeno1 <munneb.intern@devsinc.com>
2023-08-15 23:53:33 +0500

    fgfg

--- ecommerceapp/settings.py
+++ ecommerceapp/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/*-
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "django-insecure-)h20xrma_sa*@s6-)nu9_66evmvb8rr24q99q&f^)j#g%+n&@)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
